9a7b2001bb4cc54f22b0da92232bcc26152022b3
niteshrsharma <nitesh.r.sharma999@gmail.com>
2024-09-28 20:32:17 +0530

    this is update

---
+++ assignmentThree/test.py
@@ -0,0 +1,16 @@
+#assingment operator
+# = 
+
+
+# a=45
+
+# print(a+5)
+
+# relational operators
+
+# == != > < >= <=
+num=int(input("enter number"))
+if(num%2 == 0):
+    print("even")
+else:
+    print("odd")
